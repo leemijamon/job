270467113dff39bd8ff57619d1e9ced15ea3607a
leemija <leemejamon@gmail.com>
2022-03-23 10:44:13 +0900

    퍼블리싱 자료 사이트 #백업파일 붙여넣기

---
+++ page/js/main.js
@@ -0,0 +1,14 @@
+// Tab Menu
+$(function(){	
+	$('.tab_cont > div').hide();
+	$('.tab_cont > div:first-child').show();
+	
+	$('.tab_menu li a').click(function(e){
+		e.preventDefault();
+		var tab_id = $(this).attr('href');
+		$('.tab_menu li').removeClass('on');
+		$(this).parent().addClass('on');
+		$('.tab_cont > div:visible').hide();
+		$(tab_id).show();
+	});
+});
