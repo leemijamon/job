37fb8e1ac6ba78f0029ca31d77c6e7ab2536f962
이미자 <leemejamon@gmail.com>
2022-11-15 14:24:01 +0900

    gulp 환경셋팅 - 포트 5022 연동 X

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express'); // express 외부 모듈 불러오기
+const GulpClient = require('gulp');
+const nodemon = require('gulp-nodemon');
 const app = express(); // 변수 생성
 
 app.set('views', `${__dirname}/project/dist/`); // 화면은 dist 폴더에 있는 파일을 보도록
@@ -6,7 +8,7 @@ app.engine('html', require('ejs').renderFile); // ejs 엔진을 이용하여 htm
 app.set('view engine', 'html');
 
 // dist 폴더를 기본으로 하며, 그 안의 index 파일을 기본으로 띄우기
-app.use('', express.static(`${__dirname}/project/dist/`))
+app.use('', express.static(`${__dirname}/project/dist/`));
 app.get( '/' , ( req , res ) => {
 	res.render( 'index' , {}) ;
 });
@@ -14,4 +16,4 @@ app.get( '/' , ( req , res ) => {
 // 설정한 포트로 서버 띄우기, 서버가 정상작동하면 포트명 알림
 const server = app.listen( 5020, () => {
     console.log( 'Express listening on port : ' + server.address().port );
-})
+});
--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,41 @@
 const gulp = require('gulp') //gulp 모듈 불러오기
 const nodemon = require('gulp-nodemon') // 프로젝트 내의 파일에 변경사항이 있으면 서버 재시작
+const browserSync = require('browser-sync') // 소스가 변경되면 브라우저 새로고침 browser sync
 
+// html 불러오기
+gulp.task('html', () => {
+    return gulp.src('./src' + '/*.html') // src 폴더의 파일들을 읽고, task 안의 내용들을 처리한다음 dist 폴더로 재생성
+        .pipe(gulp.dest('./dist'))
+        .pipe(browserSync.reload({stream: true}))
+}); 
+
+// app.js 기반으로 서버 실행
+gulp.task('nodemon', cd => {
+    let started = false;
+    return nodemon({
+        script: 'app.js'
+    }).on('start', () => {
+     if(!started) {
+        cd();
+        started = true
+     };
+    });
+});
+
+// browser-sync task에서 nodemon을 불러와 보다 위에 있으면 안됨
+gulp.task('browser-sync',
+    gulp.series('nodemon', () => {
+        browserSync.init(null, {
+            proxy: 'http://localhost:5020',
+            port: 5022
+        })
+    })
+);
+
+// src폴더에 있는 html 소스가 변경되는지 감시
+gulp.task('watch', () => {
+    gulp.watch('./src' + '/**/*.html', gulp.series(['html']))
+});
+
+// 상단에 작성한 task 실행
+gulp.task('default', gulp.series(['html', 'browser-sync', 'watch']));
